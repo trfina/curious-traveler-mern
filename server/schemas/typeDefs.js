a29dc52bd70d886cb69542152210fed63f0e4e88
Tammy Fina <finatammy@gmail.com>
2022-04-04 16:05:16 -0400

    user/thought typeDefs and reactions

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,12 +1,69 @@
 // import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
-// create our typeDefs
 const typeDefs = gql`
-type Query {
-    helloWorld: String
-}
-`;
 
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
+
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactions: [Reaction]
+  }
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+
+    type Query {
+      thoughts:[Thought]
+    }
+
+    type Query {
+      thoughts(username: String): [Thought]
+    }
+
+    type Query {
+      users: [User]
+      user(username: String!): User
+      thoughts(username: String): [Thought]
+      thought(_id: ID!): Thought
+    }
+
+    query {
+      thoughts {
+        _id
+        username
+        thoughtText
+        createdAt
+      }
+    }
+
+    query {
+      thoughts {
+        _id
+        username
+        thoughtText
+        reactions {
+          _id
+          createdAt
+          username
+          reactionBody
+        }
+      }
+    }
+
+`;
 // export the typeDefs
 module.exports = typeDefs;
