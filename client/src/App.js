d03ee3a21d59b842107db5004236f647c90671f7
Gregory Hawkins <91228189+gthawkins24@users.noreply.github.com>
2022-04-07 19:57:54 -0400

    graphql and react server now both work on same server

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ import Profile from './pages/Profile';
 import Signup from './pages/Signup';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
+  uri: '/graphql',
 });
 
 const client = new ApolloClient({
