cb47a75533853504ff6a2d73eefc09f5e62e722e
Tammy Fina <finatammy@gmail.com>
2022-04-07 14:02:33 -0400

    Revert "routes added"

--- client/src/components/ThoughtList/index.js
+++ client/src/components/ThoughtList/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const ThoughtList = ({ thoughts, title }) => {
     if (!thoughts.length) {
@@ -13,24 +12,15 @@ return (
         thoughts.map(thought => (
         <div key={thought._id} className="card mb-3">
             <p className="card-header">
-                <Link
-                    to={`/profile/${thought.username}`}
-                    style={{ fontWeight: 700 }}
-                    className="text-light"
-                >
-                    {thought.username}
-                </Link>{' '}
-                thought on {thought.createdAt}
+            {thought.username}
+            thought on {thought.createdAt}
             </p>
-
             <div className="card-body">
-            <Link to={`/thought/${thought._id}`}>
-                <p>{thought.thoughtText}</p>
-                <p className="mb-0">
-                    Reactions: {thought.reactionCount} || Click to{' '}
-                    {thought.reactionCount ? 'see' : 'start'} the discussion!
-                </p>
-            </Link>
+            <p>{thought.thoughtText}</p>
+            <p className="mb-0">
+                Reactions: {thought.reactionCount} || Click to{' '}
+                {thought.reactionCount ? 'see' : 'start'} the discussion!
+            </p>
             </div>
         </div>
         ))}
