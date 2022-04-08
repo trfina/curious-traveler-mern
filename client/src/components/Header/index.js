505669a23f78b7d62eee126f5ca3dbe55f8d98b7
Charles DeBrosse <cmdebrosse@outlook.com?>
2022-04-07 21:50:25 -0400

    recommit on new branch

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,18 +1,22 @@
-
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import ribbonLogo from "../../assets/ribbon.png";
 
 const Header = () => {
   return (
-    <header className="bg-secondary mb-4 py-2 flex-row align-center">
+    <header className="bg-primary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <Link to="/">
-          <h1>Curious Traveler</h1>
+          <img className="logo " src={ribbonLogo} />
         </Link>
 
         <nav className="text-center">
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Signup</Link>
+          <button className="btn p-2 m-2">
+            <Link to="/login">Login</Link>
+          </button>
+          <button className="btn p-2 m-2">
+            <Link to="/signup">Signup</Link>
+          </button>
         </nav>
       </div>
     </header>
