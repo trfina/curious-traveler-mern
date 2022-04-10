7ccd3fa73d623a59c73bf19ec32e9e998ac158da
Tammy Fina <finatammy@gmail.com>
2022-04-09 21:19:22 -0400

    added alt attributes to images due to react error

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -10,7 +10,7 @@ const Profile = () => {
   const { username: userParam } = useParams();
 
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
-    variables: { username: userParam },
+    variables: { username: userParam }
   });
 
   const user = data?.me || data?.user || {};
@@ -37,17 +37,20 @@ const Profile = () => {
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
-          Viewing {userParam ? `${user.username}'s` : "your"} profile.
+          Viewing {userParam ? `${user.username}'s` : "your"} travel profile
         </h2>
+        
       </div>
-
+      
       <div className="flex-row justify-space-between mb-3">
-        <div className="col-12 mb-3 col-lg-8">
+        {/* the following div element is not rendering */}
+      <div className="col-12 mb-3 col-lg-8">
           <ThoughtList
             thoughts={user.thoughts}
-            title={`${user.username}'s thoughts...`}
+            title={`${user.username}'s curious destinations...`}
           />
-        </div>
+      </div>
+
         <div className="col-12 col-lg-3 mb-3">
           <FriendList
             username={user.username}
