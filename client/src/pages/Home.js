d889bc62e85996a5f7e6a04e487384ecfa329041
Cheri M <cmerrym1@kent.edu>
2022-04-10 00:50:40 -0400

    add friends/post working(sorta)

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,6 +4,7 @@ import FriendList from "../components/FriendList";
 import { useQuery } from "@apollo/client";
 import { QUERY_THOUGHTS, QUERY_ME_BASIC } from "../utils/queries";
 import Auth from "../utils/auth";
+import ThoughtForm from '../components/ThoughtForm';
 
 const Home = () => {
   const loggedIn = Auth.loggedIn();
@@ -17,7 +18,12 @@ const Home = () => {
   return (
     <main>
       <div className="flex-row justify-space-between">
-        <div className={`col-12 mb-3 ${loggedIn && "col-lg-8"}`}>
+        {loggedIn && (
+          <div className="col-12 mb-3">
+            <ThoughtForm />
+          </div>
+        )}
+        <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
           {loading ? (
             <div>Loading...</div>
           ) : (
