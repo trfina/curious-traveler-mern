5c7ee2fe84f7c7f5e9a3425048fa0683eddb5820
Tammy Fina <finatammy@gmail.com>
2022-04-10 17:45:32 -0400

    mutation resolver.js query_me update

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -26,7 +26,7 @@ const handleFormSubmit = async event => {
     const { data } = await login({
       variables: { ...formState }
     });
-  
+    console.log(data);
     Auth.login(data.login.token);
   } catch (e) {
     console.error(e);
