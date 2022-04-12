3d56a56444d19584e94be9779f5a1e12076e0e3c
Tammy Fina <finatammy@gmail.com>
2022-04-12 16:44:15 -0400

    Deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/curious-traveler', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/curious-traveler', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
