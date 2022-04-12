2a74eea52dd21d90f470ad951f77134c6f04484c
Tammy Fina <finatammy@gmail.com>
2022-04-12 16:55:19 -0400

    testing login

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/curious-traveler', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://trfina:test123@cluster0.e3lwb.mongodb.net/curious-traveler?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
