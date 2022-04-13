770a146c06e441e75787fff2fa47f235df3b4340
Gregory Hawkins <91228189+gthawkins24@users.noreply.github.com>
2022-04-13 19:59:52 -0400

    deploy 6

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,12 @@
-import React from "react";
-import {
-  ApolloProvider,
-  ApolloClient,
-  InMemoryCache,
-  createHttpLink,
-} from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
 import background from "./assets/map.png";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./pages/Login";
 import NoMatch from "./pages/NoMatch";
 import SingleThought from "./pages/SingleThought";
